ec86e51761d7b25b3c9f97d20e827fa1509db88b
chen <suchen@megaease.com>
2025-01-15 16:40:17 +0800

    add logger

---
+++ src/logger/__init__.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+
+def _get_logger():
+    log_level = os.environ.get("EASEVOICE_LOG_LEVEL", "WARNING")
+    log_level = log_level.upper()
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s %(filename)s:%(lineno)d - %(message)s'
+    )
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    return logger
+
+
+logger = _get_logger()
