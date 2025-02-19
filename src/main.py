852c927b636a2c9c759652c964be001a34f67a49
Yun Long <longdeqidao@gmail.com>
2025-02-19 10:10:19 +0800

    Only watch files under src/

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 if __name__ == "__main__":
-    uvicorn.run("rest.rest:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("rest.rest:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=[os.path.join(os.getcwd(), "src")])
