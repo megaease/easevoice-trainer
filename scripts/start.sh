4bd3a9e5576b288963e61fcb011d92033333c873
LokiWager <lokitingyi@gmail.com>
2025-02-27 20:09:08 -0500

    fix Docker file

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,14 @@
 
 echo "Starting trainer..."
 
-uv run src/main.py &
+
+cd /megaease/easevoice-trainer
+python src/main.py &
 
 echo "Starting Jupyter Lab..."
 
+cd /
 jupyter lab --allow-root --no-browser --port=8888 --ip=0.0.0.0 --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token="$JUPYTER_TOKEN" --ServerApp.allow_origin=* --ServerApp.preferred_dir="/root"  --app-dir="/usr/local/share/jupyter/lab"
 echo "Jupyter Lab Started"
+
+sleep infinity
